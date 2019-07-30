5dc866354341df36a5435b3edd6e00bc5f5576d4
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2019-07-30 18:07:34 +0300

    add socket.io and express

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var messages = [
   { name: "Tim", message: "Hi" },
@@ -12,6 +17,16 @@ app.get("/messages", (req, res) => {
   res.send(messages);
 });
 
-var server = app.listen(3000, () => {
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  io.emit("message", req.body);
+  res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+});
+
+var server = http.listen(3000, () => {
   console.log(`Server is listening on port: ${server.address().port}`);
 });
